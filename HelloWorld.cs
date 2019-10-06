4ccbaceb8b1976cee6f893f64f2f41439c0d204b
simtimj <juliussimtim@gmail.com>
2019-10-05 22:11:16 -0700

    Implement first user input reciever

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -5,5 +5,7 @@ class Program
   static void Main(string[] args) 
   {
     Console.WriteLine("Hello, World!");
+    string word = Console.ReadLine();
+    Console.WriteLine($"You said {word}");
   }
 }
